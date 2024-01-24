27e859964fbec3bba93cb130dac4f4e6e27e6927
Sharain7 <sharainlmc7@gmail.com>
2024-01-24 18:06:45 +0530

    fixed the connection string and updated gitIgnore

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
 const mongoose  = require('mongoose');
+require('dotenv').config();
+const dbConnectionString = process.env.DB_CONNECTION_STRING;
 const {Schema} = mongoose  ;
 
-mongoose.connect("mongodb+srv://admin:123456780@cluster0.ovji0ym.mongodb.net/paytmDB")
+mongoose.connect(dbConnectionString);
 
 
 //creating the user scehema
@@ -9,11 +11,26 @@ const userSchema = new Schema({
     username: String ,
     firstName: String ,
     lastName: String , 
-    password: String 
+    password: String ,
+    account: [{type: Schema.Types.ObjectId, ref: "Account"}]
 })
 
 //converting the schema to the model
 const User = mongoose.model('User' , userSchema)
 module.exports = {
 	User
+};
+
+//2. Creating the Account Schema for Users
+const accountSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId ,
+        ref: "User" , 
+
+    },
+    balance: Number
+})
+const Account = mongoose.model('Account' , accountSchema)
+module.exports = {
+	Account
 };
