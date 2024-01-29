9cae7f97333bf72b69dd8d0018d2f31929a3f10e
Sharain7 <sharainlmc7@gmail.com>
2024-01-29 20:45:45 +0530

    fixed the balance render

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,14 +1,45 @@
-export const Balance = ({balance}) => {
-    return(
-        <div className="flex justify-start">
-            <div className="font-medium text-lg">
-                Your balance{balance}
-            </div>
-            <div className="ml-4 text-lg">
-                Rs. {balance}
-            </div>
-            
-        </div>
-    )
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { GenericMessage } from "./GenericMessage";
 
-}
+export const Balance = () => {
+  const [balance, setBalance] = useState("Loading..."); // Default message while loading
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if(token){
+            const response = await axios.get("http://localhost:3000/api/v1/account/balance" , {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            //updating the balance
+            setBalance(response.data.balance);
+            
+
+        }
+        else{
+            <GenericMessage message={"Token not found"}/>
+
+        }
+        
+
+      } catch (e) {
+        console.log(e);
+        <GenericMessage message={"Cannot update the balance"}/>
+
+      }
+    };
+
+    fetchBalance();
+  }, [balance]);
+
+  return (
+    <div className="flex justify-start">
+      <div className="font-medium text-lg">Your balance</div>
+      <div className="ml-4 text-lg">Rs. {balance}</div>
+    </div>
+  );
+};
