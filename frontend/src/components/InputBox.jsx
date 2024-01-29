00179ddb957244b0788d8420dd29e76be949f6c5
Sharain7 <sharainlmc7@gmail.com>
2024-01-29 20:46:58 +0530

    designed a common input box component

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,10 +1,10 @@
-export const InputBox = ({label,placeholder}) =>{
+export const InputBox = ({label,placeholder,onChange}) =>{
     return (
         <div>
             <div className="font-medium text-left py-2">
                 {label}
             </div>
-            <input className="w-full px-2 py-1 border rounded border-slate-200" placeholder={placeholder}></input>
+            <input required onChange={onChange} className="w-full px-2 py-1 border rounded border-slate-200" placeholder={placeholder}></input>
 
         </div>
     )
